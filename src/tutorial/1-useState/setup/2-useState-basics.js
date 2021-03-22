9e9e60da3754ea62c955c4903b5e9309061338b7
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-03-22 14:55:04 +0900

    useState - Array Example

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-// useState -function
+// use
+// component name must be uppercase
+// must be in the function/component body
+// cannot call conditionally
 
 const UseStateBasics = () => {
   // console.log(useState());
@@ -15,6 +18,7 @@ const UseStateBasics = () => {
       setText("random title");
     }
   };
+
   return (
     <React.Fragment>
       <h1>{text}</h1>
