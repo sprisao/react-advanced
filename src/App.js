4f60badcd279db62fc5aacfab2e28099400dac71
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-03-23 09:06:22 +0900

    useState-object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import Setup from "./tutorial/1-useState/setup/3-useState-array";
+import React from 'react'
+import Setup from './tutorial/1-useState/setup/4-useState-object'
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Setup />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
