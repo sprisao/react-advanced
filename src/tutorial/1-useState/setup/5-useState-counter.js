c997c31bef12077b410338ba613a5a4eddca41a7
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-03-23 09:28:12 +0900

    Functional Update Form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,6 +6,15 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0)
   }
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue((prevState) => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -19,6 +28,13 @@ const UseStateCounter = () => {
         </button>
         <button className='btn' onClick={() => setValue(value + 1)}>
           increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          Increase Later
         </button>
       </section>
     </>
